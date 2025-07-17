4854587302253a3a7417837cf65d4ed971e2c93c
choiseoji <choiseoji123@naver.com>
2025-07-17 15:44:16 +0900

    feat: 전역 예외 처리기 뼈대 작성

---
+++ src/main/java/kr/readingtown/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package kr.readingtown.backend.exception;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+@RequiredArgsConstructor
+public class GlobalExceptionHandler {
+
+
+}
