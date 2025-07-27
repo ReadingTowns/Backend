87e0e0df562394a9c6c4c8a950d198d7b5d6ebb9
choiseoji <choiseoji123@naver.com>
2025-07-28 00:39:14 +0900

    feat: Security config 작성

---
+++ authentication/src/main/java/kr/co/readingtown/authentication/config/SecurityWebConfig.java
@@ -0,0 +1,34 @@
+package kr.co.readingtown.authentication.config;
+
+import kr.co.readingtown.authentication.oauth.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityWebConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/", "/login**").permitAll()
+                        .anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2 -> oauth2
+                        .loginPage("/login")
+                        .userInfoEndpoint(userInfo -> userInfo
+                                .userService(customOAuth2UserService)
+                        )
+                        .defaultSuccessUrl("/success.html", true)
+                );
+        return http.build();
+    }
+}
