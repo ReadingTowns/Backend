66bcba172309b200e826d8d675e1b2a724033de1
choiseoji <choiseoji123@naver.com>
2025-07-28 11:30:30 +0900

    feat: CustomOAuth2UserService에 멤버 저장하는 로직 추가

--- authentication/src/main/java/kr/co/readingtown/authentication/oauth/CustomOAuth2UserService.java
+++ authentication/src/main/java/kr/co/readingtown/authentication/oauth/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package kr.co.readingtown.authentication.oauth;
 
+import kr.co.readingtown.authentication.client.MemberClient;
 import kr.co.readingtown.authentication.domain.CustomOAuth2User;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -10,26 +12,37 @@ import org.springframework.stereotype.Service;
 import java.util.Map;
 
 @Service
+@RequiredArgsConstructor
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberClient memberClient;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
-        String provider = userRequest.getClientRegistration().getRegistrationId();
+        String provider = userRequest.getClientRegistration().getRegistrationId().toUpperCase();
         Map<String, Object> attributes = oAuth2User.getAttributes();
 
         String providerId = null;
-        if (provider.equals("kakao"))
+        String username = null;
+
+        if (provider.equals("KAKAO")) {
             providerId = String.valueOf(attributes.get("id"));
+            Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
+            username = (String) properties.get("nickname");
+        }
 
         // [TODO] : google일때 .get("sub")으로 providerId 저장해주기
+
+        // member 저장
+        memberClient.registerMember(provider, providerId, username);
 
         return new CustomOAuth2User(
                 attributes,
                 oAuth2User.getAuthorities(),
                 providerId,
-                provider.toUpperCase()
+                provider
         );
     }
 }
