16fae22acfb900045f00c3b28472c4f12f32a760
choiseoji <choiseoji123@naver.com>
2025-07-28 11:28:16 +0900

    feat: 멤버 등록하는 internal api 추가

---
+++ member/src/main/java/kr/co/readingtown/member/internalapi/InternalMemberController.java
@@ -0,0 +1,23 @@
+package kr.co.readingtown.member.internalapi;
+
+import kr.co.readingtown.member.domain.enums.LoginType;
+import kr.co.readingtown.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/internal/members")
+public class InternalMemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public void registerMember(
+            @RequestParam("provider")LoginType loginType,
+            @RequestParam("providerId") String loginId,
+            @RequestParam("username") String username) {
+
+        memberService.registerMember(loginType, loginId, username);
+    }
+}
