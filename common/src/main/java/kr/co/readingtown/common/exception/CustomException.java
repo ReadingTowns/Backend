3c57de4adee3e0e5d245bd8b7ff951e3f5933e80
choiseoji <choiseoji123@naver.com>
2025-07-31 00:21:21 +0900

    fix: CustomException에서 ErrorCode도 가지고 있도록 수정

--- common/src/main/java/kr/co/readingtown/common/exception/CustomException.java
+++ common/src/main/java/kr/co/readingtown/common/exception/CustomException.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 @Getter
 public class CustomException extends RuntimeException {
 
+    private final ErrorCode errorCodeEnum;
+
     private final int errorCode;
     private final String errorMessage;
 
     public <T extends Enum<T> & ErrorCode> CustomException(final T errorType) {
+        errorCodeEnum = errorType;
         errorCode = errorType.getErrorCode();
         errorMessage = errorType.getErrorMessage();
     }
