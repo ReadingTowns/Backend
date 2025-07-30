a8d9dd2622887228757742a33d79eb8935949901
choiseoji <choiseoji123@naver.com>
2025-07-31 00:21:47 +0900

    feat: UNAUTHORIZED 에러 추가

--- common/src/main/java/kr/co/readingtown/common/exception/httpError/HttpErrorCode.java
+++ common/src/main/java/kr/co/readingtown/common/exception/httpError/HttpErrorCode.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum HttpErrorCode implements ErrorCode {
 
-    INTERNAL_SERVER_ERROR(2001, "요청 처리 중 알 수 없는 오류가 발생했습니다.");
+    INTERNAL_SERVER_ERROR(2001, "요청 처리 중 알 수 없는 오류가 발생했습니다."),
+    UNAUTHORIZED(2002, "인증이 필요한 요청입니다.");
 
     private final int errorCode;
     private final String errorMessage;
