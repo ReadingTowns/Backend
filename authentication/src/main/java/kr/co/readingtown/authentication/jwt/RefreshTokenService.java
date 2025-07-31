9ea092687c3d7922b36c10912a71f60a507fc75d
choiseoji <choiseoji123@naver.com>
2025-07-31 15:59:29 +0900

    refact: static 변수로 리팩토링

--- authentication/src/main/java/kr/co/readingtown/authentication/jwt/RefreshTokenService.java
+++ authentication/src/main/java/kr/co/readingtown/authentication/jwt/RefreshTokenService.java
@@ -11,17 +11,24 @@ import java.util.concurrent.TimeUnit;
 public class RefreshTokenService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private static final String REFRESH_KEY_PREFIX = "refresh:";
+
+    private String generateRefreshTokenKey(String provider, String providerId) {
+        return REFRESH_KEY_PREFIX + provider + ":" + providerId;
+    }
 
     public void saveRefreshToken(String provider, String providerId, String token, long ttl) {
-        String key = "refresh:" + provider + ":" + providerId;
+        String key = generateRefreshTokenKey(provider, providerId);
         redisTemplate.opsForValue().set(key, token, ttl, TimeUnit.MILLISECONDS);
     }
 
     public String getRefreshToken(String provider, String providerId) {
-        return redisTemplate.opsForValue().get("refresh:" + provider + ":" + providerId);
+        String key = generateRefreshTokenKey(provider, providerId);
+        return redisTemplate.opsForValue().get(key);
     }
 
     public void deleteRefreshToken(String provider, String providerId) {
-        redisTemplate.delete("refresh:" + provider + ":" + providerId);
+        String key = generateRefreshTokenKey(provider, providerId);
+        redisTemplate.delete(key);
     }
 }
--- authentication/src/main/java/kr/co/readingtown/authentication/jwt/TokenBlacklistService.java
+++ authentication/src/main/java/kr/co/readingtown/authentication/jwt/TokenBlacklistService.java
@@ -11,14 +11,20 @@ import java.util.concurrent.TimeUnit;
 public class TokenBlacklistService {
 
     private final RedisTemplate<String, String> redisTemplate;
+    private static final String BLACKLIST_VALUE = "logout";
+    private static final String BLACKLIST_KEY_PREFIX = "blacklist:";
+
+    private String generateBlacklistKey (String token) {
+        return BLACKLIST_KEY_PREFIX + token;
+    }
 
     public void addToBlacklist(String token, long expirationMillis) {
-        String blacklistKey = "blacklist:" + token;
-        redisTemplate.opsForValue().set(blacklistKey, "logout", expirationMillis, TimeUnit.MILLISECONDS);
+        String blacklistKey = generateBlacklistKey(token);
+        redisTemplate.opsForValue().set(blacklistKey, BLACKLIST_VALUE, expirationMillis, TimeUnit.MILLISECONDS);
     }
 
     public boolean isBlacklisted(String token) {
-        String blacklistKey = "blacklist:" + token;
+        String blacklistKey = generateBlacklistKey(token);
         return Boolean.TRUE.equals(redisTemplate.hasKey(blacklistKey));
     }
 }
