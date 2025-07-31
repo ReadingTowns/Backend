6695605aac33f6dcfc0dbb51cf72de5eceb4964f
choiseoji <choiseoji123@naver.com>
2025-07-31 13:29:11 +0900

    feat: 토큰 종류 불일치 관련 exception 추가

--- authentication/src/main/java/kr/co/readingtown/authentication/exception/AuthenticationErrorCode.java
+++ authentication/src/main/java/kr/co/readingtown/authentication/exception/AuthenticationErrorCode.java
@@ -10,7 +10,8 @@ public enum AuthenticationErrorCode implements ErrorCode {
 
     TOKEN_NOT_VALID(3001, "유효하지 않은 토큰입니다."),
     NO_TOKEN_EXCEPTION(3002, "토큰이 없습니다"),
-    EXPIRED_TOKEN(3003, "만료된 토큰입니다.");
+    EXPIRED_TOKEN(3003, "만료된 토큰입니다."),
+    TOKEN_CATEGORY_MISMATCH(3004, "토큰의 종류가 일치하지 않습니다.");
 
     private final int errorCode;
     private final String errorMessage;
