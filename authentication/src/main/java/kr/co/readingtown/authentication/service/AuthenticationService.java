69b59902008789d738ef1a6e71f1d2f19547da9a
choiseoji <choiseoji123@naver.com>
2025-07-31 15:31:07 +0900

    feat: logout 서비스 코드 추가

--- authentication/src/main/java/kr/co/readingtown/authentication/service/AuthenticationService.java
+++ authentication/src/main/java/kr/co/readingtown/authentication/service/AuthenticationService.java
@@ -3,24 +3,32 @@ package kr.co.readingtown.authentication.service;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import kr.co.readingtown.authentication.exception.AuthenticationException;
+import kr.co.readingtown.authentication.jwt.RefreshTokenService;
+import kr.co.readingtown.authentication.jwt.TokenBlacklistService;
 import kr.co.readingtown.authentication.jwt.TokenProvider;
 import kr.co.readingtown.common.util.CookieUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static kr.co.readingtown.common.util.CookieUtil.ACCESS_TOKEN_COOKIE_NAME;
+import static kr.co.readingtown.common.util.CookieUtil.REFRESH_TOKEN_COOKIE_NAME;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class AuthenticationService {
 
-    private final TokenProvider tokenProvider;
     private final CookieUtil cookieUtil;
-    public static final String REFRESH_TOKEN_COOKIE_NAME = "refresh_token";
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenService refreshTokenService;
+    private final TokenBlacklistService tokenBlacklistService;
+    private final RedisTemplate<String, String> redisTemplate;
 
     public void reissue(HttpServletRequest request, HttpServletResponse response) {
 
-        // 1. 쿠키에서 refresh token 추리
+        // 1. 쿠키에서 refresh token 추출
         String refreshToken = cookieUtil.extractTokenFromCookie(request, REFRESH_TOKEN_COOKIE_NAME);
         if (refreshToken == null)
             throw new AuthenticationException.NoTokenException();
@@ -38,5 +46,18 @@ public class AuthenticationService {
 
         // 5. 쿠키에 새 token 저장
         cookieUtil.saveTokenToCookie(response, newAccessToken, newRefreshToken);
+    }
+
+    public void logout(HttpServletRequest request) {
+
+        // 1. 쿠키에서 access token 추출
+        String token = cookieUtil.extractTokenFromCookie(request, ACCESS_TOKEN_COOKIE_NAME);
+
+        // 2. redis에서 refresh token 삭제
+        refreshTokenService.deleteRefreshToken(tokenProvider.getProvider(token), tokenProvider.getProviderId(token));
+
+        // 3. redis 블랙리스트에 access token 저장
+        long expiration = tokenProvider.getExpiration(token);  // access token 남은 유효기간
+        tokenBlacklistService.addToBlacklist(token, expiration);
     }
 }
