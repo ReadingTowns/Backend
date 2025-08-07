556c9e38139c884ab11284afaceba05e5964be54
choiseoji <choiseoji123@naver.com>
2025-08-07 16:34:45 +0900

    feat: external exception 터진 경우 response 형식 추가

--- common/src/main/java/kr/co/readingtown/common/response/CommonResponse.java
+++ common/src/main/java/kr/co/readingtown/common/response/CommonResponse.java
@@ -2,6 +2,7 @@ package kr.co.readingtown.common.response;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import kr.co.readingtown.common.exception.ErrorCode;
+import kr.co.readingtown.common.exception.httpError.HttpErrorCode;
 
 import java.time.LocalDateTime;
 
@@ -18,8 +19,13 @@ public record CommonResponse<T>(
         this(LocalDateTime.now(), 1000, "Success", result);
     }
 
-    // api 응답이 실패인 경우
+    // 비즈니스 로직에서 정의한 커스텀 예외(ErrorCode) 발생 시 실패 응답 생성
     public CommonResponse(ErrorCode errorCode) {
         this(LocalDateTime.now(), errorCode.getErrorCode(), errorCode.getErrorMessage(), null);
+    }
+
+    // 시스템 또는 외부 예외(Exception) 발생 시 실패 응답 생성
+    public CommonResponse(Exception e) {
+        this(LocalDateTime.now(), 2001, e.getMessage(), null);
     }
 }
