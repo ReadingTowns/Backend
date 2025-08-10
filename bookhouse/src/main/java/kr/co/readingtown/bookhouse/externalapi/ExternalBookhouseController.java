cc2a77afca544553ea7bf2f6d0f97d44f06b5e25
choiseoji <choiseoji123@naver.com>
2025-08-10 22:55:43 +0900

    feat: bookhouse external api 추가

---
+++ bookhouse/src/main/java/kr/co/readingtown/bookhouse/externalapi/ExternalBookhouseController.java
@@ -0,0 +1,54 @@
+package kr.co.readingtown.bookhouse.externalapi;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import kr.co.readingtown.bookhouse.dto.request.BookInfoRequestDto;
+import kr.co.readingtown.bookhouse.dto.response.BookPreviewResponseDto;
+import kr.co.readingtown.bookhouse.service.BookhouseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/bookhouse")
+@Tag(name = "Bookhouse API", description = "서재 관련 API")
+public class ExternalBookhouseController {
+
+    private final BookhouseService bookhouseService;
+
+    @PostMapping("/books")
+    @Operation(summary = "서재에 책 추가", description = "현재 로그인한 회원의 서재에 특정 책을 추가합니다.")
+    public void addBooksToBookhouse(
+            @AuthenticationPrincipal Long memberId,
+            @RequestBody BookInfoRequestDto bookInfoRequestDto) {
+
+        bookhouseService.addBooksToBookhouse(memberId, bookInfoRequestDto);
+    }
+
+    @DeleteMapping("/books/{bookId}")
+    @Operation(summary = "서재에서 책 삭제", description = "현재 로그인한 회원의 서재에서 특정 책을 삭제합니다.")
+    public void deleteBookFromBookhouse(
+            @PathVariable Long bookId,
+            @AuthenticationPrincipal Long memberId) {
+
+        bookhouseService.deleteBookFromBookhouse(memberId, bookId);
+    }
+
+    @GetMapping("/members/me")
+    @Operation(summary = "내 서재 조회", description = "현재 로그인한 회원의 서재를 조회합니다.")
+    public List<BookPreviewResponseDto> getMyBookhouse(@AuthenticationPrincipal Long memberId) {
+
+        return bookhouseService.getBookhouseBooks(memberId);
+    }
+
+    @GetMapping("/members/{memberId}")
+    @Operation(summary = "회원의 서재 조회", description = "특정 회원의 서재를 조회합니다.")
+    public List<BookPreviewResponseDto> getBookhouseByMemberId(@PathVariable Long memberId) {
+
+        return bookhouseService.getBookhouseBooks(memberId);
+    }
+
+}
