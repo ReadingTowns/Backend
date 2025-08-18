c96dcf72561cf885360a1e50c810cf8ca0f88ce5
choiseoji <choiseoji123@naver.com>
2025-08-18 15:07:37 +0900

    feat: isExchanged 경로 수정

--- bookhouse/src/main/java/kr/co/readingtown/bookhouse/domain/Bookhouse.java
+++ bookhouse/src/main/java/kr/co/readingtown/bookhouse/domain/Bookhouse.java
@@ -1,6 +1,7 @@
 package kr.co.readingtown.bookhouse.domain;
 
 import jakarta.persistence.*;
+import kr.co.readingtown.bookhouse.domain.enums.IsExchanged;
 import kr.co.readingtown.common.entity.BaseEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
