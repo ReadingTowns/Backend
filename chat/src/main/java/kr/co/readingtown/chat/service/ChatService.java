82a0aaaabb9b97b4a65e06ef6411d8e6e1163a19
choiseoji <choiseoji123@naver.com>
2025-08-18 15:08:18 +0900

    feat: 채팅방에서 필요한 기능 추가

--- chat/src/main/java/kr/co/readingtown/chat/service/ChatService.java
+++ chat/src/main/java/kr/co/readingtown/chat/service/ChatService.java
@@ -2,6 +2,11 @@ package kr.co.readingtown.chat.service;
 
 import kr.co.readingtown.chat.domain.Chatroom;
 import kr.co.readingtown.chat.dto.request.ChatRequestDto;
+import kr.co.readingtown.chat.dto.response.*;
+import kr.co.readingtown.chat.exception.ChatException;
+import kr.co.readingtown.chat.integration.book.BookClient;
+import kr.co.readingtown.chat.integration.bookhouse.BookhouseClient;
+import kr.co.readingtown.chat.integration.member.MemberClient;
 import kr.co.readingtown.chat.repository.ChatroomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,25 +15,56 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ChatService {
 
+    public final BookClient bookClient;
+    public final MemberClient memberClient;
+    public final BookhouseClient bookhouseClient;
+
     public final ChatroomRepository chatroomRepository;
 
-    // 기존 채팅룸이 존재하면 반환, 없으면 생성
-    // 요청한 책에 대해 교환 상태 하나 생성
-    public Long createChatroom(ChatRequestDto chatRequestDto, Long memberId) {
 
-        // 채팅방 조회
+    // 교환하고자 하는 책의 서재에서 채팅 요청
+    public ChatroomIdResponse createChatroom(ChatRequestDto chatRequestDto, Long memberId) {
+
         return chatroomRepository.findChatroomIdByMemberIds(chatRequestDto.memberId(), memberId)
-                .orElseGet(() -> {
-                    // 새 채팅방 생성
-                    Chatroom newChatroom = Chatroom.builder()
-                            .ownerId(chatRequestDto.memberId())
-                            .requesterId(memberId)
-                            .build();
+                .map(ChatroomIdResponse::new)
+                .orElseGet(() -> createNewChatroom(chatRequestDto, memberId));
+    }
 
-                    // 교환 상태 생성
+    private ChatroomIdResponse createNewChatroom(ChatRequestDto chatRequestDto, Long memberId) {
 
+        Chatroom newChatroom = Chatroom.builder()
+                .ownerId(chatRequestDto.memberId())
+                .requesterId(memberId)
+                .build();
 
-                    return chatroomRepository.save(newChatroom).getChatroomId();
-                });
+        // 교환 상태 생성 (bookhouse 모듈 호출)
+        bookhouseClient.createExchangeStatus(chatRequestDto);
+
+        Long chatroomId = chatroomRepository.save(newChatroom).getChatroomId();
+        return new ChatroomIdResponse(chatroomId);
+    }
+
+    // 채팅룸 교환 책 정보 조회
+    public ChatExchangedBookInfoResponse getExchangedBookInfo(Long chatroomId) {
+
+        ExchangedBookResponse exchangedBooks = bookhouseClient.getBookIdByChatroomId(chatroomId);
+
+        BookInfoResponse myBookInfo = bookClient.getBookInfo(exchangedBooks.myBook().bookId());
+        BookInfoResponse partnerBookInfo = bookClient.getBookInfo(exchangedBooks.partnerBook().bookId());
+
+        return ChatExchangedBookInfoResponse.from(exchangedBooks, myBookInfo, partnerBookInfo);
+    }
+
+    // 채팅룸 상대방 정보 조회
+    public ChatMemberInfoResponse getPartnerInfo(Long chatroomId, Long myId) {
+
+        Chatroom chatroom = chatroomRepository.findById(chatroomId)
+                .orElseThrow(ChatException.ChatroomNotFound::new);
+
+        Long partnerId = chatroom.getOwnerId().equals(myId)
+                ? chatroom.getRequesterId()
+                : chatroom.getOwnerId();
+
+        return memberClient.getMemberInfo(partnerId);
     }
 }
