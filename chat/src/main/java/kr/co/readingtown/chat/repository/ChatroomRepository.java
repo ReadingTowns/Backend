2196f0533b50485b641bb03390c02dcb22335eb5
choiseoji <choiseoji123@naver.com>
2025-08-21 01:12:11 +0900

    feat: 내가 속한 채팅룸 조회하는 레포지토리 코드 추가

--- chat/src/main/java/kr/co/readingtown/chat/repository/ChatroomRepository.java
+++ chat/src/main/java/kr/co/readingtown/chat/repository/ChatroomRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
@@ -16,4 +17,12 @@ public interface ChatroomRepository extends JpaRepository<Chatroom, Long> {
         AND cr.requesterId = :requesterId
     """)
     Optional<Long> findChatroomIdByMemberIds(@Param("ownerId") Long ownerId, @Param("requesterId")Long requesterId);
+
+    @Query("""
+    SELECT cr
+    FROM Chatroom cr
+    WHERE cr.requesterId = :memberId
+        OR cr.ownerId = :memberId
+    """)
+    List<Chatroom> findMyChatrooms(@Param("memberId") Long memberId);
 }
