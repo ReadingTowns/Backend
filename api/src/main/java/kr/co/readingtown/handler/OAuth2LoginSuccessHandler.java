85e8d3511b48f5ac0e154eab0f0b684c80b89ed5
choiseoji <choiseoji123@naver.com>
2025-10-02 15:59:37 +0900

    feat: 리다이렉트 수정

--- api/src/main/java/kr/co/readingtown/handler/OAuth2LoginSuccessHandler.java
+++ api/src/main/java/kr/co/readingtown/handler/OAuth2LoginSuccessHandler.java
@@ -32,6 +32,6 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
 
         cookieUtil.saveTokenToCookie(response, accessToken, refreshToken);
 
-        response.sendRedirect("http://localhost:3000/home");
+        response.sendRedirect("https://readingtowns.vercel.app/home");
     }
 }
