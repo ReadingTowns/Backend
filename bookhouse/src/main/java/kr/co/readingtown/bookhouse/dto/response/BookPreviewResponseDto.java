4b1efc9efe7d7283f33e983490703c850e040c20
고여경 <2yokyung@naver.com>
2025-10-12 21:38:15 +0900

    fix: 서재 책 리스트 조회 API 응답 bookhouseId 추가

--- bookhouse/src/main/java/kr/co/readingtown/bookhouse/dto/response/BookPreviewResponseDto.java
+++ bookhouse/src/main/java/kr/co/readingtown/bookhouse/dto/response/BookPreviewResponseDto.java
@@ -1,6 +1,7 @@
 package kr.co.readingtown.bookhouse.dto.response;
 
 public record BookPreviewResponseDto(
+        Long bookhouseId,
         Long bookId,
         String bookImage,
         String bookName,
