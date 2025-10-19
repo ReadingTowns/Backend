1fd300a493850e5409e729178b90b7f75e0f5a4f
choiseoji <choiseoji123@naver.com>
2025-10-19 19:07:46 +0900

    feat: 유저가 선택한 키워드의 content만 조회하는 repository 코드 추가

--- member/src/main/java/kr/co/readingtown/member/repository/KeywordRepository.java
+++ member/src/main/java/kr/co/readingtown/member/repository/KeywordRepository.java
@@ -2,6 +2,18 @@ package kr.co.readingtown.member.repository;
 
 import kr.co.readingtown.member.domain.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface KeywordRepository extends JpaRepository<Keyword, Long> {
+
+    @Query("""
+    SELECT k.content
+    FROM Keyword k
+        JOIN MemberKeyword mk ON mk.keywordId = k.keywordId
+    WHERE mk.memberId = :memberId
+    """)
+    List<String> findContentsByMemberId(@Param("memberId") Long memberId);
 }
