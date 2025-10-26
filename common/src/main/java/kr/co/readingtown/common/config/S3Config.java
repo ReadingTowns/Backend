634ab06c002d3fe16698e1ed3dc10b5b25499ff8
고여경 <2yokyung@naver.com>
2025-10-27 00:35:21 +0900

    chore: AWS SDK JMX 메트릭 비활성화로 JDK cgroup NullPointerException 해결
    - AWS SDK 초기화 시 JMX MBean 등록 과정에서 jdk.internal.platform.CgroupInfo.getMountPoint() 호출로 인한 NPE가 발생하던 문제 해결
    - S3Config AwsSdkMetrics.unregisterMetricAdminMBean() 호출을 추가하여 컨테이너 환경에서 불필요한 시스템 메트릭 등록을 비활성화함.

--- common/src/main/java/kr/co/readingtown/common/config/S3Config.java
+++ common/src/main/java/kr/co/readingtown/common/config/S3Config.java
@@ -2,8 +2,10 @@ package kr.co.readingtown.common.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.metrics.AwsSdkMetrics;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,6 +21,21 @@ public class S3Config {
 
     @Value("${aws.s3.region}")
     private String region;
+
+    // 애플리케이션 시작 시 AWS SDK의 JMX/metrics 등록을 비활성화
+    @PostConstruct
+    public void disableAwsSdkJmx() {
+        try {
+            // JVM의 컨테이너 감지 비활성화
+            System.setProperty("jdk.internal.platform.disableContainerSupport", "true");
+
+            // AWS SDK의 JMX MBean 등록 비활성화
+            AwsSdkMetrics.unregisterMetricAdminMBean();
+
+        } catch (Throwable ignored) {
+            // 에러 무시 (JMX 비활성화만 목적)
+        }
+    }
 
     @Bean
     public AmazonS3 getAmazonS3Client() {
